2be2106fc9d1c438ea232399b0ca9b68bae9014d
camtothebirk <92412830+camtothebirk@users.noreply.github.com>
2021-12-10 22:00:47 -0500

    Add files via upload

--- Student.sql
+++ Student.sql
@@ -1,1 +1,11 @@
-SELECT * FROM DataTest.Student;
+INSERT INTO `DataTest`.`Student`
+(`idStudent`,
+`Name`,
+`Surname`)
+VALUES
+(9088,'Cam','Birkeland'),
+(5674,'Bob','Carr'),
+(0512,'Darrell','Dickson'),
+(7325,'Joel','Green');
+
+
